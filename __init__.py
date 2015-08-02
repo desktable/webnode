58e997989571a2fc53694e8f39368365edb2fe61
Henry Chen <chendaizhuo@gmail.com>
2015-08-02 19:56:50 -0400

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""Python module to generate HTML, CSS, and JavaScript for webpages
+
+This module is developed under the influence of Quora's Webnode2 system:
+http://www.quora.com/Quora-Infrastructure/What-is-Webnode2
+"""
+
+from __future__ import unicode_literals
+
+from . import html
+from .component import Component
