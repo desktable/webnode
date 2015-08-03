55241e12c64862fe27ca627b7d33179454cf6855
Henry Chen <chendaizhuo@gmail.com>
2015-08-02 20:13:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 
 from distutils.core import setup
 
-setup(name='Webnode',
+setup(name='webnode',
       version='0.1',
-      description='Python Distribution Utilities',
+      description='Write HTML in Python',
       author='Daizhuo Chen',
       author_email='chendaizhuo@gmail.com',
       url='https://github.com/desktable/webnode',
