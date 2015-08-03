0dc07a17e61dfc543b82a7b20cbeee48072ac025
Henry Chen <chendaizhuo@gmail.com>
2015-08-02 20:07:42 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Webnode',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Daizhuo Chen',
+      author_email='chendaizhuo@gmail.com',
+      url='https://github.com/desktable/webnode',
+      packages=['webnode'],
+      )
